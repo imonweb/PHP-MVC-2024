8b8dc2928de2ea38b7fdf9c4636f1f12ccd6bfda
Imon <contact@imonweb.co.uk>
2024-10-28 22:31:39 +0000

    initial commit

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+function show($stuff)
+{
+  echo "<pre>";
+  print_r($stuff);
+  echo "</pre>";
+}
+
+function splitURL()
+{ 
+  $URL = $_GET['url'] ?? 'home';
+  $URL = explode("/", $URL);
+  return $URL;
+}
+
+function loadController()
+{
+  $URL = splitURL();
+
+  $filename = "../app/controllers/".$URL[0].".php";
+  if(file_exists($filename))
+  {
+    require $filename;
+  } else {
+    $filename = "../app/controllers/_404.php";
+    require $filename;
+  }
+}
+
+loadController();
+// show(splitURL());
